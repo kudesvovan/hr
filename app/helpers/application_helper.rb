0c0cd8f8ddcadd6378e668048ed211e85e4762b0
vladimir vasilyev <vladi_it@mail.ru>
2015-04-14 22:08:22 +0300

    fixed some err after contacts method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,27 @@ module ApplicationHelper
 		x.skills.each {|skill| list << skill.name}
 		list.join(', ')
   end
+
+  
+  def contacts(employee)
+    email = employee.email
+    phone = employee.phone
+    contacts = []
+
+    # почты нет, тел есть
+    if email.nil? && !phone.nil?
+    	contacts << phone
+    # почты нет, тел нет
+    elsif email.nil? && phone.nil?
+    	contacts
+    # почта есть, тел нет
+    elsif !email.nil? && phone.nil?
+    	contacts << email
+    # почта есть, тел есть
+    elsif !email.nil? && !phone.nil?
+    	contacts << email
+    	contacts << phone
+    end
+    contacts.join(', ')
+  end
 end
--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,25 +1,4 @@
 module EmployeesHelper
 	
-  def contacts(employee)
-    email = employee.email
-    phone = employee.phone
-    contacts = []
-
-    # почты нет, тел есть
-    if email.nil? && !phone.nil?
-    	contacts << phone
-    # почты нет, тел нет
-    elsif email.nil? && phone.nil?
-    	contacts
-    # почта есть, тел нет
-    elsif !email.nil? && phone.nil?
-    	contacts << email
-    # почта есть, тел есть
-    elsif !email.nil? && !phone.nil?
-    	contacts << email
-    	contacts << phone
-    end
-    contacts.join(', ')
-  end
 	
 end
